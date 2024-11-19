310b1443a105e7090245609359ba026165b2ab2a
Muhammadfareed <ff063063063@gmail.com>
2024-11-19 21:09:44 +0500

    complete

---
+++ src/content_option.js
@@ -0,0 +1,143 @@
+const logotext = "Fareed";
+const meta = {
+    title: "Muhammad Fareed",
+    description: "I’m Fareed  _ Full stack devloper,currently working in Pakistan/Karachi",
+};
+
+const introdata = {
+    title: "I’m Muhammad Fareed",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+const skills = [
+    {
+        name: "HTML 5",
+        value: 99,
+    },
+    {
+        name: "CSS 3",
+        value: 90,
+    },
+    {
+        name: "BootStrap ",
+        value: 90,
+    },
+    {
+        name: "Tailwind CSS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Redux Toolkit",
+        value: 50,
+    },
+    {
+        name: "React Native",
+        value: 85,
+    },
+    {
+        name: "Node,js",
+        value: 60,
+    },
+    {
+        name: "Express,js",
+        value: 60,
+    },
+    {
+        name: "MongoDB",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: require("./assets/images/pro_1.png"),
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: require("./assets/images/pro-2.png"),
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: require("./assets/images/pro_3.jpg"),
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: require("./assets/images/pro_4.webp"),
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: require("./assets/images/pro_5.jpg"),
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: require("./assets/images/pro_6.png"),
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ff063063063@gmail.com",
+    YOUR_PHONE: "03041025570",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_gy3wx75",
+    YOUR_TEMPLATE_ID: "template_kaymuhg",
+    YOUR_USER_ID: "wBuvL5DASvn8dfHAJ",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
